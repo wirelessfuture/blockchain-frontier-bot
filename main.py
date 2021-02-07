a6763e22c7dae075c370b45e82cc883976116201
wirelessfuture <wirelessfuture2000@gmail.com>
2021-02-07 15:34:59 +0100

    Update comments

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-"""
-Simple Bot to reply to Telegram messages.
-First, a few handler functions are defined. Then, those functions are passed to
-the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
 import os
 
