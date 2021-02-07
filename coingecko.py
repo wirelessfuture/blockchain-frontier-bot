d342eafe174b91290f3d56d12d2cb03a23528b63
wirelessfuture <wirelessfuture2000@gmail.com>
2021-02-07 20:25:46 +0100

    Add BTC Price and Percentage

--- coingecko.py
+++ coingecko.py
@@ -12,20 +12,38 @@ def check_server(func: any) -> any:
     return wrapper_is_server_ok
 
 @check_server
-def get_eth_price() -> dict:
-    """Get the latest price of Ethereum in various currencies."""
-    return cg.get_price(ids='ethereum', vs_currencies='usd,gbp,czk,aud')
-
-@check_server
 def get_trending_search() -> dict:
     """Get trending search coins (Top-7) on CoinGecko in the last 24 hours."""
     return cg.get_search_trending()
 
 @check_server
-def get_percentage_data() -> dict:
-    """Get the latest percentage data in various time ranges."""
+def get_eth_price() -> dict:
+    """Get the latest price of Ethereum in various currencies."""
+    return cg.get_price(ids='ethereum', vs_currencies='usd,gbp,czk,aud')
+
+@check_server
+def get_eth_percentage_change() -> dict:
+    """Get the latest Ethereum price percentage change in various time ranges."""
     return cg.get_coin_by_id(
         id="ethereum", 
+        localization_string=False, 
+        tickers=False, 
+        market_data=True, 
+        community_data=False, 
+        developer_data=False, 
+        sparkline=False
+    )
+
+@check_server
+def get_btc_price() -> dict:
+    """Get the latest price of Bitcoin in various currencies."""
+    return cg.get_price(ids='bitcoin', vs_currencies='usd,gbp,czk,aud')
+
+@check_server
+def get_btc_percentage_change() -> dict:
+    """Get the latest Bitcoin price percentage change in various time ranges."""
+    return cg.get_coin_by_id(
+        id="bitcoin", 
         localization_string=False, 
         tickers=False, 
         market_data=True, 
