d4f2a0c5948a692d3b6c2565875ae2fc7735d2b1
wirelessfuture <wirelessfuture2000@gmail.com>
2021-02-07 16:49:51 +0100

    Add new trending command

--- main.py
+++ main.py
@@ -16,25 +16,36 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Import the price checker
-from price import get_eth_price
+from coingecko import get_eth_price, get_trending_search
 
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Use the command /price to check the price of Eth!')
-
+    update.message.reply_text('Use the command /price to check the price of Eth!\nUse the command /trending to get the latest trending searches on Coingecko!')
 
 def price_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /price is issued."""
     new_price = get_eth_price()
-    usd = new_price["ethereum"]["usd"]
-    gbp = new_price["ethereum"]["gbp"]
-    czk = new_price["ethereum"]["czk"]
-    aud = new_price["ethereum"]["aud"]
-    update.message.reply_text(f'Eth USD: ${usd}\nEth GBP: £{gbp}\nEth CZK: {czk}Kč\nEth AUD: ${aud}')
+    if "error" not in new_price:
+        usd = new_price["ethereum"]["usd"]
+        gbp = new_price["ethereum"]["gbp"]
+        czk = new_price["ethereum"]["czk"]
+        aud = new_price["ethereum"]["aud"]
+        update.message.reply_text(f'Eth USD: ${usd}\nEth GBP: £{gbp}\nEth CZK: {czk}Kč\nEth AUD: ${aud}')
+    else:
+        update.message.reply_text(new_price["error"])
 
+def trending_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /trending is issued."""
+    new_trending = get_trending_search()
+    if "error" not in new_trending:
+        tokens = [token["item"]["name"] + "\n" for token in new_trending["coins"]]
+        tokens.insert(0, "Top 7 Searches on Coingecko:\n")
+        update.message.reply_text(f''.join(tokens))
+    else:
+        update.message.reply_text(new_trending["error"])
 
 def main():
     """Start the bot."""
@@ -47,6 +58,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("price", price_command))
+    dispatcher.add_handler(CommandHandler("trending", trending_command))
 
     # Start the Bot
     updater.start_polling()
