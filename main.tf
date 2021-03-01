d43ee38852f0d7f0f4eb8f5c7bfe38ddf2bdbb07
wirelessfuture <wirelessfuture2000@gmail.com>
2021-03-01 22:53:01 +0100

    Add files, provisioner and connection for launch script

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
+      source = "hashicorp/aws"
     }
   }
 }
 
 # Providers
+# AWS
 provider "aws" {
   profile = "default"
   region  = "eu-central-1"
@@ -18,8 +18,39 @@ provider "aws" {
 # AWS Lightsail instance
 resource "aws_lightsail_instance" "bf_lightsail" {
   name              = "blockchain_frontier_bot"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1a"
   blueprint_id      = "ubuntu_20_04"
   bundle_id         = "micro_2_0"
   key_pair_name     = "blockchain-frontier-bot"
+
+  provisioner "file" {
+    source      = "launch-script.sh"
+    destination = "/tmp/launch-script.sh"
+  }
+
+  provisioner "file" {
+    source      = "docker-pull.sh"
+    destination = "/tmp/docker-pull.sh"
+  }
+
+  provisioner "file" {
+    source      = "docker-pass.txt"
+    destination = "/tmp/docker-pass.txt"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/launch-script.sh && chmod +x /tmp/docker-pull.sh",
+      "sudo /tmp/launch-script.sh",
+      "sudo /tmp/docker-pull.sh",
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    password    = ""
+    private_key = file(var.keyPath)
+    host        = self.ipv6_address
+  }
 }
