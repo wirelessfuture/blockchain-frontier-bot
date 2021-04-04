b6dc562c3c92057879c787d205bbfcf552ebe9a0
wirelessfuture <wirelessfuture2000@gmail.com>
2021-04-04 13:12:52 +0200

    Update gigachad list

--- src/coingecko.py
+++ src/coingecko.py
@@ -4,14 +4,14 @@ cg = CoinGeckoAPI()
 
 def check_server(func: any) -> any:
     """Decorator for checking the coingecko API status before each request"""
-    def wrapper_is_server_ok():
+    def wrapper_is_server_ok(*args, **kwargs):
         try:
             if cg.ping()["gecko_says"] == "(V3) To the Moon!":
-                return func()
+                return func(*args, **kwargs)
             else:
                 return {"error": "Server is currently experiencing issues"}
-        except Exception:
-            return {"error": "Server is currently experiencing issues"}
+        except Exception as e:
+            return {"error": "Could not find coin with the given id"}
     return wrapper_is_server_ok
 
 @check_server
@@ -20,49 +20,10 @@ def get_trending_search() -> dict:
     return cg.get_search_trending()
 
 @check_server
-def get_eth_market_data() -> dict:
-    """Get the latest Ethereum market data."""
+def get_market_data(id: str) -> dict:
+    """Get the latest market data."""
     return cg.get_coin_by_id(
-        id="ethereum", 
-        localization_string=False, 
-        tickers=False, 
-        market_data=True, 
-        community_data=False, 
-        developer_data=False, 
-        sparkline=False
-    )
-
-@check_server
-def get_btc_market_data() -> dict:
-    """Get the latest Bitcoin market data."""
-    return cg.get_coin_by_id(
-        id="bitcoin", 
-        localization_string=False, 
-        tickers=False, 
-        market_data=True, 
-        community_data=False, 
-        developer_data=False, 
-        sparkline=False
-    )
-
-@check_server
-def get_ttt_market_data() -> dict:
-    """Get the latest Tapcoin market data."""
-    return cg.get_coin_by_id(
-        id="tapcoin", 
-        localization_string=False, 
-        tickers=False, 
-        market_data=True, 
-        community_data=False, 
-        developer_data=False, 
-        sparkline=False
-    )
-
-@check_server
-def get_ada_market_data() -> dict:
-    """Get the latest Bitcoin market data."""
-    return cg.get_coin_by_id(
-        id="cardano", 
+        id=id, 
         localization_string=False, 
         tickers=False, 
         market_data=True, 
@@ -78,7 +39,8 @@ def get_gigachad_prices() -> dict:
         "xio",
         "mettalex",
         "origintrail",
-        "rocket-pool"
+        "rocket-pool",
+        "flash-stake"
     ]
     price_dict = {}
     for token in gigachad_list:
