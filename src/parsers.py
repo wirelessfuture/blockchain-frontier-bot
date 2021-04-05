424913c1f1ed42dd2adce04c6f22aab68eee5e45
wirelessfuture <wirelessfuture2000@gmail.com>
2021-04-06 00:17:41 +0200

    Add fun emojis

--- src/parsers.py
+++ src/parsers.py
@@ -3,17 +3,17 @@ def market_data_parser(market_data: dict, coin: str) -> list:
     # Current Prices
     currency_keys = ["aud", "usd", "czk", "gbp"]
     current_prices = [f'{key.upper()}: {value}\n' for key, value in market_data["current_price"].items() if key in currency_keys]
-    current_prices.insert(0, f'Current {coin} Price\n')
+    current_prices.insert(0, f'📈 Current {coin} Price 📉\n')
     current_prices.append('\n')
 
     # 24 Hour Price Change
     price_change_percentage_24h = [f'{key.upper()}: {value}%\n' for key, value in market_data["price_change_percentage_24h_in_currency"].items() if key in currency_keys]
-    price_change_percentage_24h.insert(0, '24h Price Change %\n')
+    price_change_percentage_24h.insert(0, '⏱ 24h Price Change % 📊\n')
     price_change_percentage_24h.append('\n')
 
     # 24 Hour Percentage Change
     price_change_24h = [f'{key.upper()}: {value}\n' for key, value in market_data["price_change_24h_in_currency"].items() if key in currency_keys]
-    price_change_24h.insert(0, '24h Price Change\n')
+    price_change_24h.insert(0, '⏱ 24h Price Change 📊\n')
     price_change_24h.append('\n')
 
     # Return Concatenated Lists
@@ -23,7 +23,7 @@ def eth_gas_price_parser(gas_data: dict) -> list:
     """Takes a dict with gas price data and parses the result as a list."""
     price_keys = ["average", "fast", "fastest"]
     gas_prices = [f'{key}: {int(value/10)} GWEI\n' for key, value in gas_data.items() if key in price_keys]
-    gas_prices.insert(0, 'ETH Gas Prices\n')
+    gas_prices.insert(0, '⛽️ ETH Gas Prices ⛽️\n')
     gas_prices.append('\n')
 
     return gas_prices
